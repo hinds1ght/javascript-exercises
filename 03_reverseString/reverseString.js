f5ffc784b3c0d7697922fd86cc25030c77d719d9
hinds1ght <markgil_yu@yahoo.com>
2023-02-05 15:06:18 +0800

    passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let arr = str.split("");
+let x = arr.length - 1;  
+let test = ''; 
+        for (i = x; i >= 0; i--){
+            test += arr[i];
+        }
+        return test;
 };
 
 // Do not edit below this line

10172abf34fada0bab4c2669c2e3caa64f83ea82
hinds1ght <markgil_yu@yahoo.com>
2023-02-05 16:36:33 +0800

    passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+let total = parseInt(1);
+    for (i = 1; i < y; i++){
+        x += 1;
+        total += x;
+    } 
+    return total;
 };
 
 // Do not edit below this line

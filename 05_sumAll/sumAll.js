1a076ae60447ba4c43b2b810bef6f9ef20c159cc
hinds1ght <markgil_yu@yahoo.com>
2023-02-05 17:41:04 +0800

    passed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,23 @@
 const sumAll = function(x, y) {
+    if (typeof(y) !== "number" || x < 0){
+        return 'ERROR';
+    }
+    let a = y;
+    let b = x;
+        if(x > y){
+            b = y;
+            a = x;
+        }
 let total = parseInt(1);
-    for (i = 1; i < y; i++){
-        x += 1;
-        total += x;
+    for (i = 1; i < a; i++){
+        b += 1;
+        total += b;
     } 
-    return total;
+    if (total == 1){
+        return 'ERROR';
+    }else{
+        return total;
+    }
 };
 
 // Do not edit below this line
